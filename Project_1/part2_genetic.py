4a91738c4abc11dd89e4461218d9bd845e65ed0c
Max Li <mla0212@str.corp>
2018-02-04 16:45:21 -0500

    added keeping track of t ime

--- Project_1/part2_genetic.py
+++ Project_1/part2_genetic.py
@@ -1,6 +1,6 @@
 import numpy
 import part2 as p2 # This import is only until we actually incorporate together.
-import random
+import random,time
 
 IND = 100
 COM = 200
@@ -20,15 +20,22 @@ pick two states.
 execute 
 '''
 
-def geneticStateSearch(mapIn,iCount,cCount,rCount, timeRun):
+def geneticStateSearch(mapIn,iCount,cCount,rCount, timeToRun):
 	k = 100
-	k2 = floor(k/20)	
+	k2 = numpy.floor(k/20)	
 	numCull = 5 ### Or maybe make it so that it's a threshold
 	prob_mutate = 0.06
 
 	numRows = mapIn.shape[0]
 	numCols = mapIn.shape[1]
 
+	initTime = time.time()
+	timeRun = 0.0
+	while timeRun < timeToRun:
+		# Go through the actual process of generating children and picking and mutation etc
+		
+		# Update the current time
+		timeRun = time.time() - initTime
 
 
 '''
@@ -37,3 +44,4 @@ Part 2 genetic testing
 random.seed()
 
 mapIn,iCount,cCount,rCount = p2.readFile('sample2.txt')
+geneticStateSearch(mapIn,iCount,cCount,rCount, 1)
