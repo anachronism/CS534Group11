50673ab5eadd151cbec02761b26d66c03b174813
ilyaL1 <joellif@gmail.com>
2018-02-04 14:33:32 -0500

    added building values

--- Project_1/part2.py
+++ Project_1/part2.py
@@ -4,7 +4,6 @@
 
 import numpy
 
-
 Matrix2 = numpy.zeros((6, 5))
 
 #function takes in map(state) calculates score of the map
@@ -14,28 +13,39 @@ def calculateStateScore(state):
 	print(state)
 	print(columns,rows)
 	totalScore=0
+	
+	iList = []
+	cList = []
 	rList = []
+ 	tList = []
+ 
 	for j in range(columns):
 		for i in range(rows):
-			if (state[i,j] == 50):
+			if (state[i,j] == 100):
 				iList.append((i,j))
-			if (state[i,j] == 60):
+			if (state[i,j] == 200):
 				rList.append((i,j))
-			if (state[i,j] == 70):
-				rList.append((i,j))
-			
-	print("rlist",rList) test 
+			if (state[i,j] == 300):
+				cList.append((i,j))
+	print(len(iList))		
+	#for j in range(len(iList))
+	print("rlist",rList)
 
 
 def getManhDist(loc1,loc2):
 	 distance = abs(loc1[0]-loc2[0])+abs(loc1[1]-loc2[1])
 	 return distance
 
+ind = 100
+com = 200
+res = 300
+toxic = 400
 
-Matrix2[2,1] = 50
-Matrix2[5,4] = 50
-Matrix2[1,4] = 60
-Matrix2[3,2] = 70
+Matrix2[2,1] = ind
+Matrix2[5,4] = com
+Matrix2[1,4] = res
+Matrix2[3,2] = toxic
+
 calculateStateScore(Matrix2)    
 
 
