ff250730adfbe3240772ee9e0b93dc356e86b4bb
Max Li <mla0212@str.corp>
2018-02-04 19:11:57 -0500

    finished random state generation in genetic algorithm

--- Project_1/part2_genetic.py
+++ Project_1/part2_genetic.py
@@ -9,9 +9,7 @@ TOXIC = 400
 SCENIC = 500
 
 
-# generateRandomState()
 # randomCrossover()
-
 
 '''
 Start: k list of random, valid states.
@@ -19,8 +17,7 @@ calculate fitness scores for these k states.
 pick two states.
 execute 
 '''
-
-def geneticStateSearch(mapIn,iCount,cCount,rCount, timeToRun):
+def geneticStateSearch(mapIn,iCount,cCount,rCount,listAvoid,timeToRun):
 	k = 100
 	k2 = numpy.floor(k/20)	
 	numCull = 5 ### Or maybe make it so that it's a threshold
@@ -29,11 +26,51 @@ def geneticStateSearch(mapIn,iCount,cCount,rCount, timeToRun):
 	numRows = mapIn.shape[0]
 	numCols = mapIn.shape[1]
 
-	initTime = time.time()
+	# Gen locations is a iCount+cCount+rCount x k matrix containing tuples
+	genLocations = numpy.zeros((k,iCount+cCount+rCount), dtype=(int,2))
+	#tmpListAvoid = numpy.ones(iCount+cCount+rCount,dtype=(int,2))
+
+	firstRun = True
 	timeRun = 0.0
+	initTime = time.time()
+
 	while timeRun < timeToRun:
 		# Go through the actual process of generating children and picking and mutation etc
 		
+		if firstRun == True:
+			cnt_avoid = 0
+			# Generate  k states randomly	
+			for i in range(0,k):
+				tmpListAvoid = list(listAvoid)
+				for j in range(0,iCount+cCount+rCount):
+					valid = False
+					while valid == False:
+						tmpLocation= (random.randint(0,numRows),random.randint(0,numCols))#genLocations[i,j] 
+						# Check location's validity.
+						# print tmpLocation
+						#print(tmpListAvoid)
+						if tmpLocation in tmpListAvoid:
+							cnt_avoid = cnt_avoid + 1
+							#print('avoiding',cnt_avoid)
+							pass
+						else:
+							# leave the while and add current location to random.
+							valid = True
+							tmpListAvoid.append(tmpLocation)
+							genLocations[i,j] = tmpLocation
+
+			# print(genLocations[1,:])
+			firstRun = False
+		else:
+			pass
+			# Select k2 most fit states to save
+			# cull from last gen
+			# Use crossover etc to make k-k2 states
+				# weight towards states w/ better fitness.
+				# Combining 2 states makes 2 successors.
+				# Randomly change some bits in some states.
+
+
 		# Update the current time
 		timeRun = time.time() - initTime
 
@@ -42,6 +79,11 @@ def geneticStateSearch(mapIn,iCount,cCount,rCount, timeToRun):
 Part 2 genetic testing
 '''
 random.seed()
-
+listAvoid = [];
 mapIn,iCount,cCount,rCount = p2.readFile('sample2.txt')
-geneticStateSearch(mapIn,iCount,cCount,rCount, 1)
+toxic_sites = numpy.where(mapIn == TOXIC)
+for elt,ind in enumerate(toxic_sites[0]):
+	listAvoid.append((toxic_sites[0][ind],toxic_sites[1][ind])) 
+
+print(listAvoid[0][:]) # somehow make this into something that works with the other thing.
+geneticStateSearch(mapIn,iCount,cCount,rCount,listAvoid, 1)
