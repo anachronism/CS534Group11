60eef23e93deaee7520be0ed6f81368249a13605
ilyaL1 <joellif@gmail.com>
2018-03-13 23:58:27 -0400

    small bug correction

--- Project_2/gibbsSampler.py
+++ Project_2/gibbsSampler.py
@@ -262,7 +262,7 @@ while "=" in inputString[i]:
     tempList = BAYESMAP[tempEvidenceNode[0]].possibleValues
     tempIndex = tempList.index(tempEvidenceNode[1])
     print "\n index:", tempIndex, "\n"
-    BAYESMAP[tempEvidenceNode[0]].value = tempEvidenceNode[1]    
+    BAYESMAP[tempEvidenceNode[0]].value = tempIndex[1]    
     i=i+1
 
 updateNumber = int(inputString[i+1])
